298c38c189df39ca3a4671249b1df4efcaff8b54
Wei Sun <weisun2013@gmail.com>
2021-01-25 10:58:36 -0800

    update TCGA SKCM analysis

--- R/SKCM_step1_CARseq.R
+++ R/SKCM_step1_CARseq.R
@@ -26,7 +26,7 @@ names(cell_sizes) = c("Bcells", "CAFs", "CD4_Tcells", "CD8_Tcells",
                       "Endothelial", "Macrophages", "NKcells")
 
 trec_file_name = "../data/SKCM_trec.rds"
-covariate_file_name = "../data/SKCM_dat_cavariates_with_svs.rds"
+covariate_file_name = "../data/SKCM_cavariates.rds"
 cell_fractions_file_name = "../data/SKCM_cell_fraction.rds"
 
 # ------------------------------------------------------------------------
@@ -250,7 +250,6 @@ if(cluster_tss){
 cdat$tss = as.factor(cdat$tss)
 table(cdat$tss, tss)
 
-# if (!file.exists(covariate_file_name)) {
 # ------------------------------------------------------------------------
 # generate covariate data
 # ------------------------------------------------------------------------
@@ -261,236 +260,10 @@ col_data = cdat[, c("age_at_initial_pathologic_diagnosis",
 col_data$scaled_age = scale(col_data$age_at_initial_pathologic_diagnosis)
 col_data$scaled_log_depth = scale(cdat$log_depth)
 
+dim(col_data)
+col_data[1:2,]
+
 cor(model.matrix(~ gender + scaled_age + scaled_log_depth + stage, col_data)[,-1])
-
-# ----------------------------------------------------------------------
-# first run PCA on residuals to help determin the number of PCs to use
-# ----------------------------------------------------------------------
-
-prop = cell_fractions
-dat  = col_data
-
-dim(prop)
-prop[1:2,]
-
-dim(dat)
-dat[1:2,]
-
-plog = log(prop + 1e-5)
-plog = plog - plog[,"Cancer_cells"]
-colnames(plog) = paste("log", colnames(plog), sep="_")
-plog = plog[, !grepl("log_Cancer_cells", colnames(plog))]
-
-dat = cbind(dat, plog)
-dim(dat)
-dat[1:2,]
-
-mod0.terms = c("gender", "scaled_age", "scaled_log_depth", "stage", 
-               "tss", colnames(plog))
-length(mod0.terms)
-
-mod0.str = paste(mod0.terms, collapse = " + ")
-
-# Looping over genes for lm() and residuals
-GG = nrow(trec); NN = ncol(trec)
-rr = matrix(NA,GG,NN) # residual matrix
-
-for(gg in seq(GG)){
-  # gg = 1
-  if(gg %% 1e2 == 0) cat(".")
-  if(gg %% 2e3 == 0 || gg == GG) cat(sprintf("%s out of %s\n",gg,GG))
-  
-  log.trec.gg = log(trec[gg,] + 1)
-  
-  lm_out = lm(formula(sprintf("log.trec.gg ~ %s", mod0.str)), data = dat)
-  rr[gg,] = as.numeric(lm_out$residuals)
-  aa = drop1(lm_out,.~.,test="F")
-  
-  if(gg == 1){
-    pp = matrix(NA,GG,nrow(aa)-1)
-    colnames(pp) = rownames(aa)[-1]
-  }
-  
-  pp[gg,] = aa[-1,6]
-  rm(lm_out)
-}
-
-pdf(file.path("..", "figures", "SKCM_expression_PCA_all_log_Prop.pdf"),
-    height = 8,width = 8)
-
-show_pvalue_hist(mat_pvalues = pp, test_type0 = 3)
-
-rr2 = rr - rowMeans(rr,na.rm = TRUE)
-cov_rr2 = t(rr2) %*% rr2 / nrow(rr2); pca_rr2 = eigen(cov_rr2)
-
-show_screeplot(pca_rr2,main = "")
-par(mfrow = c(2,1),mar = c(4,4,1,1),oma = c(0,0,2,0))
-barplot(pca_rr2$values[3:21], names.arg =3:21, main = "", 
-        xlab = "Index", ylab = "Eigen-value")
-barplot(diff(-pca_rr2$values[3:22]), names.arg =3:21, main = "", 
-        xlab = "Index", ylab = "Eigen-value[i] - Eigen-value[i+1]")
-
-num_pcs = 5; pcs = smart_df(pca_rr2$vectors[,1:num_pcs])
-names(pcs) = paste0("PC",seq(num_pcs))
-show_pc_color(pcs,submain = "")
-
-dev.off()
-
-pcs = pca_rr2$vectors[,1:20]
-cor(pcs, dat$five_year_DSS)
-
-# ----------------------------------------------------------------------
-# SVA
-# ----------------------------------------------------------------------
-
-mod.terms = c("five_year_DSS", mod0.terms)
-length(mod.terms)
-
-mod0 = model.matrix(as.formula(paste("~", paste(mod0.terms, collapse=" + "))), 
-                    data=dat)
-mod  = model.matrix(as.formula(paste("~", paste(mod.terms, collapse=" + "))), 
-                    data=dat)
-
-dim(mod0)
-mod0[1:2,]
-
-dim(mod)
-mod[1:2,]
-
-n.sv = num.sv(log(trec + 1), mod, method="leek")
-print(n.sv)
-
-n.sv = num.sv(log(trec + 1), mod, method="be")
-print(n.sv)
-
-sv4 = sva(log(trec + 1), mod, mod0, n.sv=4)
-sv8 = sva(log(trec + 1), mod, mod0, n.sv=8)
-
-
-# ----------------------------------------------------------------------
-# check associatoin between PCs and svs
-# ----------------------------------------------------------------------
-
-pcs = pca_rr2$vectors[,1:8]
-npc = 1:8
-round(cor(sv4$sv, pcs), 2)
-round(cor(sv8$sv, pcs), 2)
-round(cor(sv4$sv, sv8$sv), 2)
-
-r2 = matrix(NA, nrow=8, ncol=length(npc))
-for(k in 1:8){
-  svk = as.numeric(sv8$sv[,k])
-  for(m in 1:length(npc)){
-    lmk = summary(lm(svk ~ pcs[,1:npc[m]]))
-    r2[k,m] = lmk$r.squared
-  }
-}
-
-round(r2, 2)
-
-pdf(file.path("..","figures", "SKCM_svs_vs_pcs.pdf"),
-    height = 6,width = 6)
-par(mar=c(5,4,1,1), bty="n")
-for(i in 1:nrow(r2)){
-  if(i==1){
-    plot(npc, r2[1,], ylim=c(0,1), type="l", xlim=c(0,9), 
-         xlab="# of PCs", ylab="R2 of surrogate variables explained by PCs")
-  }else{
-    lines(npc, r2[i,])
-  }
-}
-abline(v=4, lty=2, col="darkred")
-abline(v=8, lty=2, col="darkred")
-text(rep(9,8), r2[,8], labels=1:8)
-dev.off()
-
-# ----------------------------------------------------------------------
-# R squared vs. the number of surrogate variables to use
-# ----------------------------------------------------------------------
-
-colnames(sv8$sv) = paste0("sv", 1:8)
-dat = cbind(dat, sv8$sv)
-dim(dat)
-dat[1:2,]
-
-log_trec = log(trec + 1)
-final.mod0.terms = mod0.terms
-final.mod0.str = paste(final.mod0.terms, collapse = " + ")
-
-max_number_of_SV = 8
-final.mod0.str.vector = rep(NA, max_number_of_SV + 1)
-final.mod0.str.vector[1] = final.mod0.str
-
-for (number_of_SV in (seq_len(max_number_of_SV))) {
-  final.mod0.str.vector[1 + number_of_SV] = 
-    sprintf("%s + sv%d", final.mod0.str.vector[number_of_SV], number_of_SV)
-}
-
-GG = nrow(log_trec); NN = ncol(log_trec)
-rr = matrix(NA,GG,NN) # residual matrix
-
-rsquared = matrix(NA, nrow = GG, ncol = 1 + max_number_of_SV)
-for(gg in seq_len(GG)){
-  # gg = 1
-  if(gg %% 1e2 == 0) cat(".")
-  if(gg %% 2e3 == 0 || gg == GG) cat(sprintf("%s out of %s\n",gg,GG))
-  
-  log_trec.gg = log_trec[gg,]
-  
-  # starts from 0
-  for (number_of_SV in (-1 + seq_len(max_number_of_SV + 1))) {
-    lm_out = lm(formula(sprintf("log_trec.gg ~ %s", 
-                                final.mod0.str.vector[1 + number_of_SV])), 
-                data = dat)
-    rsquared[gg, 1 + number_of_SV] = summary(lm_out)$r.squared
-    rm(lm_out)
-  }
-}
-
-colnames(rsquared) = paste0("SV",  -1 + seq_len(max_number_of_SV + 1))
-
-saveRDS(rsquared, file.path("..", "results", "SKCM_rsquared.rds"))
-
-colMedians(rsquared, na.rm=TRUE)
-colnames(rsquared) = sprintf("%s\n%.2f", 
-                             paste0("SV",  -1 + seq_len(max_number_of_SV + 1)), 
-                             colMedians(rsquared, na.rm=TRUE))
-
-png(file.path("..", "figures", "SKCM_rsquared.png"),
-    width=9, height=5, units="in", res=400)
-par(mar = c(6.5, 3.0, 0.5, 1.0))
-boxplot(rsquared, sub = paste(strwrap(paste("log_trec ~", final.mod0.str), 110), 
-                              collapse = "\n"))
-dev.off()
-
-round(cor(sv8$sv, cell_fractions),2)
-
-# ----------------------------------------------------------------------
-# save results
-# ----------------------------------------------------------------------
-
-mod[1:2,]
-colnames(mod)
-
-idx1 = which(colnames(mod) =="log_Bcells") - 1
-idx1
-
-modDat = cbind(mod[,2:idx1], sv8$sv)
-colnames(modDat)[1:2] = c("five_year_DSS", "gender") 
-colnames(modDat)[idx1:(idx1+7)] = paste0("sv", 1:8)
-dim(modDat)
-modDat[1:2,]
-
-# rescale PCs and SVs -- the names are still sv1, sv2, etc, 
-# but they have been scaled:
-covariates_to_scale = grep("sv|PC", colnames(modDat))
-for (m in covariates_to_scale) {
-  modDat[, m] = scale(modDat[, m])
-}
-
-col_data = modDat
-saveRDS(col_data, file=covariate_file_name)
-
 
 # permuted disease label
 set.seed(1234)
@@ -499,13 +272,12 @@ col_data$SurvivalP = CARseq:::permute_case_and_controls(col_data$five_year_DSS)
 col_data$Survival  = factor(col_data$five_year_DSS)
 table(col_data$Survival, col_data$SurvivalP)
 
-# CARseq, CIBERSORT, 0 SV
-# permuted
+saveRDS(col_data, file = covariate_file_name)
+
+# CARseq using permuted response variable
 date()
 
-str1 = setdiff(colnames(modDat), c("five_year_DSS", paste0("sv", 1:8)))
-str1 = paste(str1, collapse=" + ")
-f1 = as.formula(paste("~", str1))
+f1 = ~ gender + scaled_age + scaled_log_depth + stage + tss
 f1
 
 res_CARseq = run_CARseq(count_matrix = trec,
@@ -521,9 +293,8 @@ res_CARseq = run_CARseq(count_matrix = trec,
 )
 date()
 
-saveRDS(res_CARseq, "../results/SKCM_CARseq_CIBERSORT_permuted_SV0.rds")
-pdf("../figures/SKCM_CARseq_CIBERSORT_permuted_SV0.pdf",
-    width=6, height=6)
+saveRDS(res_CARseq, "../results/SKCM_CARseq_permuted.rds")
+pdf("../figures/SKCM_CARseq_permuted.pdf", width=9, height=5)
 par(mfrow=c(2,4), bty="n", mar=c(5,4,2,1))
 for(k in seq_len(ncol(res_CARseq$p))) {
   sk = sum(CARseq:::get_qvalues_one_inflated(res_CARseq$p[, k]) < 0.1, na.rm=TRUE)
@@ -534,8 +305,7 @@ dev.off()
 
 
 
-# CARseq, CIBERSORT, 0 SV
-# not permuted
+# CARseq, not permuted
 date()
 
 res_CARseq = run_CARseq(count_matrix = trec,
@@ -551,9 +321,9 @@ res_CARseq = run_CARseq(count_matrix = trec,
 )
 date()
 
-saveRDS(res_CARseq, "../results/SKCM_CARseq_CIBERSORT_SV0.rds")
+saveRDS(res_CARseq, "../results/SKCM_CARseq.rds")
 
-pdf("../figures/SKCM_CARseq_CIBERSORT_SV0.pdf", width=6, height=6)
+pdf("../figures/SKCM_CARseq.pdf", width=9, height=5)
 par(mfrow=c(2,4), bty="n", mar=c(5,4,2,1))
 for(k in seq_len(ncol(res_CARseq$p))) {
   sk = sum(CARseq:::get_qvalues_one_inflated(res_CARseq$p[, k]) < 0.1, na.rm=TRUE)
@@ -562,64 +332,6 @@ for(k in seq_len(ncol(res_CARseq$p))) {
 }
 dev.off()
 
-f1 = as.formula(paste("~", str1, " + sv1"))
-f1
-# CARseq, CIBERSORT, 1 SV
-# permuted
-date()
-
-res_CARseq = run_CARseq(count_matrix = trec,
-                        cellular_proportions = cell_fractions,
-                        groups = col_data$SurvivalP, 
-                        formula = f1, 
-                        data = col_data, 
-                        read_depth = 1, 
-                        shrunken_lfc = TRUE, 
-                        cores = 15, 
-                        fix_overdispersion = FALSE,
-                        useSocket = FALSE
-)
-date()
-
-saveRDS(res_CARseq, "../results/SKCM_CARseq_CIBERSORT_permuted_SV1.rds")
-
-pdf("../figures/SKCM_CARseq_CIBERSORT_permuted_SV1.pdf", width=6, height=6)
-par(mfrow=c(2,4), bty="n", mar=c(5,4,2,1))
-for(k in seq_len(ncol(res_CARseq$p))) {
-  sk = sum(CARseq:::get_qvalues_one_inflated(res_CARseq$p[, k]) < 0.1, na.rm=TRUE)
-  hist(res_CARseq$p[,k], main=colnames(res_CARseq$p)[k], breaks=20,
-       xlab = paste0("p-value (", sk, " genes w/ q<0.1)"))
-}
-dev.off()
-
-
-# CARseq, CIBERSORT, 1 SV
-# not permuted
-date()
-
-res_CARseq = run_CARseq(count_matrix = trec,
-                        cellular_proportions = cell_fractions,
-                        groups = col_data$Survival,
-                        formula = f1,
-                        data = col_data,
-                        read_depth = 1,
-                        shrunken_lfc = TRUE,
-                        cores = 15, 
-                        fix_overdispersion = FALSE,
-                        useSocket = FALSE
-)
-date()
-
-saveRDS(res_CARseq, "../results/SKCM_CARseq_CIBERSORT_SV1.rds")
-
-pdf("../figures/SKCM_CARseq_CIBERSORT_SV1.pdf", width=6, height=6)
-par(mfrow=c(2,4), bty="n", mar=c(5,4,2,1))
-for(k in seq_len(ncol(res_CARseq$p))) {
-  sk = sum(CARseq:::get_qvalues_one_inflated(res_CARseq$p[, k]) < 0.1, na.rm=TRUE)
-  hist(res_CARseq$p[,k], main=colnames(res_CARseq$p)[k], breaks=20,
-       xlab = paste0("p-value (", sk, " genes w/ q<0.1)"))
-}
-dev.off()
 
 gc()
 
